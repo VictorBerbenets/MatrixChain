d9bb4d450c33bb08a2fb814eac50cebf417e3bd5
Victor Berbenets <berbenets.vd@phystech.edu>
2023-10-15 13:09:43 +0300

    changed vector on deque, made push back-front methods

--- include/matrix_chain.hpp
+++ include/matrix_chain.hpp
@@ -1,7 +1,7 @@
 #ifndef MATRIX_CHAIN
 #define MATRIX_CHAIN
 
-#include <vector>
+#include <deque>
 #include <iterator>
 #include <utility>
 
@@ -12,19 +12,21 @@ namespace yLAB {
 template<numeric_type T>
 class MatrixChain final {
 public:
-    using size_type              = std::size_t;
     using value_type             = T;
     using matrix_type            = Matrix<value_type>;
     using reference              = matrix_type&;
     using const_reference        = const matrix_type&;
     using pointer                = matrix_type*;
     using const_pointer          = const matrix_type*;
-    using iterator               = typename std::vector<matrix_type>::iterator;
-    using const_iterator         = typename std::vector<matrix_type>::const_iterator;
-    using reverse_iterator       = typename std::vector<matrix_type>::reverse_iterator;
-    using const_reverse_iterator = typename std::vector<matrix_type>::const_reverse_iterator;
+    using iterator               = typename std::deque<matrix_type>::iterator;
+    using const_iterator         = typename std::deque<matrix_type>::const_iterator;
+    using reverse_iterator       = typename std::deque<matrix_type>::reverse_iterator;
+    using const_reverse_iterator = typename std::deque<matrix_type>::const_reverse_iterator;
+    using size_type              = typename std::deque<matrix_type>::size_type;
 private:
-    
+    using check_pair = std::pair<const_iterator, bool>;
+
+    enum class InsertPos : char { Front = 1, Back = 2};
 public:
     constexpr MatrixChain() = default;
 
@@ -41,16 +43,56 @@ public:
     constexpr MatrixChain(std::initializer_list<matrix_type> ls)
     : MatrixChain(ls.begin(), ls.end()) {}
     
-    constexpr void add(const matrix_type& matrix) {
-        chain_.push_back(matrix_type(matrix));
+    constexpr check_pair push_back(const matrix_type& matrix) {
+        if (insertion_check(matrix, InsertPos::Back)) {
+            chain_.push_back(matrix);
+            return {chain_.begin(), true};
+        }
+        return {chain_.begin(), false};
     }
 
-    constexpr void add(matrix_type&& matrix) {
-        chain_.push_back(std::move(matrix));
+    constexpr check_pair push_back(matrix_type&& matrix) {
+        if (insertion_check(matrix, InsertPos::Back)) {
+            chain_.push_back(std::move(matrix));
+            return {chain_.begin(), true};
+        }
+        return {chain_.begin(), false};
     }
-    
+
+    constexpr check_pair push_front(const matrix_type& matrix) {
+        if (insertion_check(matrix, InsertPos::Front)) {
+            chain_.push_front(matrix);
+            return {chain_.begin(), true};
+        }
+        return {chain_.begin(), false};
+    }
+
+    constexpr check_pair push_front(matrix_type&& matrix) {
+        if (insertion_check(matrix, InsertPos::Front)) {
+            chain_.push_front(std::move(matrix));
+            return {chain_.begin(), true};
+        }
+        return {chain_.begin(), false};
+    }
+ 
+    size_type size() const noexcept { return chain_.size(); };
+    bool empty() const noexcept { return chain_.empty(); };
 private:
-    std::vector<matrix_type> chain_;
+    bool insertion_check(const matrix_type& matrix, InsertPos pos) const {
+        if (!size()) { return true; }
+
+        switch(pos) {
+            case InsertPos::Front:
+                return chain_.front().nline() == matrix.ncolumn();
+            case InsertPos::Back:
+                return chain_.back().ncolumn() == matrix.nline();
+            default: break;
+        }
+        return false; // we won't get here
+    }
+
+private:
+    std::deque<matrix_type> chain_;
 };
 
 }; // <--- namespace yLAB
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,18 @@
 #include "matrix_chain.hpp"
 
 int main() {
+    yLAB::MatrixChain<int> chain{};
 
+    yLAB::Matrix<int> m1 {2, 2, {1, 2, 3, 4}};
+    yLAB::Matrix<int> m2 {2, 2, {1, 2, 3, 4}};
+    yLAB::Matrix<int> m3 {2, 2, {1, 2, 3, 4}};
+    yLAB::Matrix<int> m4 {3, 2, {1, 3, 4, 5, 6, 5}};
+
+    chain.push_back(m1);
+    chain.push_back(std::move(m2));
+    chain.push_front(m3);
+    chain.push_front(std::move(m4));
+
+    std::cout << "size = " << chain.size() << std::endl;
 }
 
